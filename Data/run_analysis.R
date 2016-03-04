89cadb35d578d9f322422e23f88dd283c40dd409
Sandra <sbarbuti@gmail.com>
2016-03-03 19:21:37 -0500

    Initial commit

---
+++ Data/run_analysis.R
@@ -0,0 +1,38 @@
+#import data
+#Merges the training and the test sets to create one data set.
+#Extracts columns containing mean and standard deviation for each measurement 
+#(Hint: Since some feature/column names are repeated, you may need to use the make.names() function in R)
+#Creates variables called ActivityLabel and ActivityName that label all observations with the corresponding activity labels and names respectively
+#From the data set in step 3, creates a second, independent tidy data set with the average of each variable for each activity and each subject.library(plyr)
+
+setwd("C:/Users/Sandra/Documents/DataScienceCert/Data")
+library(plyr)
+library(dplyr)
+paths <- dir(path = "C:/Users/Sandra/Documents/DataScienceCert/Data", pattern = "\\.csv")
+names(paths) <- basename(paths)
+tennisdata <- ldply(paths, read.csv)
+list.files(paths)
+glimpse(tennisdata)
+
+
+
+tennisdata %>% select(.id) %>% distinct
+
+# of records by file
+tennisdata %>% 
+  group_by(.id) %>% 
+  summarise(count = n())
+
+#rename id var
+
+#account for missing vars -- recode?
+
+
+#mean sd
+
+tennisdata %>% 
+  #group_by(paths) %>% 
+  summarise_each(funs(mean(., na.rm=TRUE)))
+
+ausopen <- read.csv("AusOpen-men-2013.csv")
+glimpse(ausopen)
